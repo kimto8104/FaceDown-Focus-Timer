e36ae3670aa374714441b35509206b108019c21f
tomo8104 <kimto8104@gmail.com>
2023-02-12 14:56:13 +0900

    Initial Commit

---
+++ NowFocus/NowFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowFocusApp.swift
+//  NowFocus
+//
+//  Created by Tomofumi Kimura on 2023/02/12.
+//
+
+import SwiftUI
+
+@main
+struct NowFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
