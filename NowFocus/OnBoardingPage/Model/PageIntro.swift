40fdf29bc5f0b19663348b32d5f865b2c810ed3c
tomo8104 <41029872+kimto8104@users.noreply.github.com>
2023-11-02 22:01:02 +0900

    commit

---
+++ NowFocus/OnBoardingPage/Model/PageIntro.swift
@@ -0,0 +1,8 @@
+//
+//  PageIntro.swift
+//  NowFocus
+//
+//  Created by Tomofumi Kimura on 2023/05/27.
+//
+
+import Foundation
--- NowFocus/TimerPage/TimerPage.swift
+++ NowFocus/TimerPage/TimerPage.swift
@@ -7,11 +7,15 @@
 
 import SwiftUI
 import AVFoundation
+import CoreMotion
 
 struct TimerPage: View {
   @StateObject private var viewModel: TimerViewModel
   @Environment(\.presentationMode) var presentationMode
   
+  // iPhone を持ち上げたどうかを表すフラグ
+  @State private var isIphoneLifted = false
+  // モーションマネジャーインスタンス
   @State private var alertIsPresented = false
   
   init(time: Int) {
@@ -30,7 +34,11 @@ struct TimerPage: View {
           if time == 0 {
           }
         }
-        .onAppear(perform: viewModel.start)
+        .onAppear {
+          viewModel.start()
+          // 加速度センサーとジャイロスコープからのデータを取得する
+          
+        }
         .onDisappear(perform: viewModel.stop)
     }
     
@@ -53,7 +61,18 @@ struct TimerPage: View {
         Text("戻る")
       })
     )
+  } // body ここまで
+  
+  func showAlert() {
+    let alert = UIAlertController(title: "iPhoneを持ち上げました", message: "タイマーを終了しますか？", preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "戻る", style: .cancel))
+    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+      self.viewModel.stop()
+      self.presentationMode.wrappedValue.dismiss()
+    }))
+    UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true)
   }
+  
   
 }
 
