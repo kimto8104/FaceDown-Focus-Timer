354002c0eb49d516bfbc9be5e09d358c32ace1a6
tomo8104 <41029872+kimto8104@users.noreply.github.com>
2023-11-02 22:03:42 +0900

    test

--- NowFocus/OnBoardingPage/Model/PageIntro.swift
+++ NowFocus/OnBoardingPage/Model/PageIntro.swift
@@ -5,4 +5,20 @@
 //  Created by Tomofumi Kimura on 2023/05/27.
 //
 
-import Foundation
+import SwiftUI
+
+/// Page Intro Model
+
+struct PageIntro: Identifiable, Hashable {
+  var id: UUID = .init()
+  var introAssetImage: String
+  var title: String
+  var subTitle: String
+  
+}
+
+var pageIntros: [PageIntro] = [
+  .init(introAssetImage: "study_man_normal", title: "Focus for yourself", subTitle: "test is important"),
+  .init(introAssetImage: "study_man_normal", title: "Focus for yourself", subTitle: "test is important"),
+  .init(introAssetImage: "study_man_normal", title: "Focus for yourself", subTitle: "test is important")
+]
--- NowFocus/TimerPage/TimerPage.swift
+++ NowFocus/TimerPage/TimerPage.swift
@@ -9,38 +9,62 @@ import SwiftUI
 import AVFoundation
 import CoreMotion
 
+// MARK: - View
 struct TimerPage: View {
   @StateObject private var viewModel: TimerViewModel
   @Environment(\.presentationMode) var presentationMode
-  
-  // iPhone を持ち上げたどうかを表すフラグ
-  @State private var isIphoneLifted = false
-  // モーションマネジャーインスタンス
   @State private var alertIsPresented = false
-  
+ 
+  @ObservedObject private(set) var model = TimerPageVM()
   init(time: Int) {
     self._viewModel = StateObject(wrappedValue: TimerViewModel(time: time * 60))
   }
   
   var body: some View {
     ZStack {
+      // Timer の丸
       Circle()
         .stroke(Color.blue, lineWidth: 4)
         .frame(width: 200, height: 200)
+      // 残り時間
       Text(viewModel.remainingTime.asTimeString)
         .font(.largeTitle)
         .foregroundColor(.blue)
         .onReceive(viewModel.$remainingTime) { time in
           if time == 0 {
+            // time が0ならタイマーを止める
+            viewModel.stop()
           }
         }
         .onAppear {
-          viewModel.start()
-          // 加速度センサーとジャイロスコープからのデータを取得する
-          
+          // 端末の向きがFaceUpではない場合Timerスタートする
+          if model.isFaceUp == false {
+            viewModel.start()
+          }
         }
         .onDisappear(perform: viewModel.stop)
     }
+    // isFaceUpの値を監視、falseならタイマースタート
+    .onReceive(model.$isFaceUp, perform: { isFaceUp in
+      let _ = print("isFaceUp? \(isFaceUp)")
+      if model.isFaceUp == false {
+        viewModel.start()
+      }
+    })
+    
+    .onAppear {
+      // 画面の向きの見地を開始
+      model.startInspect()
+    }
+    
+    
+    .alert("デバイスの画面を下に向けてください", isPresented: $model.isFaceUp, actions: {
+      Button("OK") {
+        if model.isFaceUp == false {
+          viewModel.start()
+        }
+      }
+    })
     
     .alert(isPresented: $alertIsPresented) {
       Alert(
@@ -62,18 +86,30 @@ struct TimerPage: View {
       })
     )
   } // body ここまで
+}
+
+// MARK: - View Model
+class TimerPageVM: ObservableObject {
+  // 上向か下向きかの判定関連
+  @Published fileprivate var isFaceUp: Bool = true
+  fileprivate var orientationObserver: NSObjectProtocol? = nil
+  let notification = UIDevice.orientationDidChangeNotification
   
-  func showAlert() {
-    let alert = UIAlertController(title: "iPhoneを持ち上げました", message: "タイマーを終了しますか？", preferredStyle: .alert)
-    alert.addAction(UIAlertAction(title: "戻る", style: .cancel))
-    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-      self.viewModel.stop()
-      self.presentationMode.wrappedValue.dismiss()
-    }))
-    UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true)
+  // デバイスの向きの検知を開始
+  func startInspect() {
+    // 検知開始
+    UIDevice.current.beginGeneratingDeviceOrientationNotifications()
+    orientationObserver = NotificationCenter.default.addObserver(forName: notification, object: nil, queue: .main, using: { [weak self] _ in
+      switch UIDevice.current.orientation {
+      case .faceUp:
+        self?.isFaceUp = true
+//      case .faceDown:
+//        self?.isFaceUp = false
+      default:
+        print("other orientation")
+      }
+    })
   }
-  
-  
 }
 
 struct TimerPage_Previews: PreviewProvider {
