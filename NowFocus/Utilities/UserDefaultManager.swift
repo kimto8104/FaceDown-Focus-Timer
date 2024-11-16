a72591ff0c36f7c9fe78fd869adca0c5b4e2a2e3
tomo8104 <41029872+kimto8104@users.noreply.github.com>
2024-11-17 08:33:03 +0900

    日付が変わるとクリアした集中時間をリセットするよう実装

--- NowFocus/Utilities/UserDefaultManager.swift
+++ NowFocus/Utilities/UserDefaultManager.swift
@@ -24,6 +24,31 @@ class UserDefaultManager: NSObject {
 }
 
 extension UserDefaultManager {
+  
+  // 前回のチェック日を取得・更新
+  static var lastCheckedDate: Date {
+    get { return UserDefaults.standard.object(forKey: #function) as? Date ?? Date() }
+    set { UserDefaults.standard.set(newValue, forKey: #function) }
+  }
+  
+  // 日付が変わっていればデイリーデータをリセット
+  static func resetDailyDataIfDateChanged() {
+    let today = Calendar.current.startOfDay(for: Date())
+    let lastChecked = Calendar.current.startOfDay(for: lastCheckedDate)
+    // 最後の確認日と今日の日付を比較し、変わっていればリセット
+    if today > lastChecked {
+      resetAllDate()
+    }
+  }
+  
+  private static func resetAllDate() {
+    oneMinuteDoneToday = false
+    tenMinuteDoneToday = false
+    fifteenMinuteDoneToday = false
+    thirtyMinuteDoneToday = false
+    fiftyMinuteDoneToday = false
+  }
+  
   // 1分を今日完了したか？
   static var oneMinuteDoneToday: Bool {
     get { return boolForKey(#function) }
@@ -53,8 +78,6 @@ extension UserDefaultManager {
     get { return boolForKey(#function) }
     set { setBool(newValue, forKey: #function) }
   }
-  
-  
 }
 
 
