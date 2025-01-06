ed3028fbdc486fd546b6ebe5c87cfebde13c5967
tomo8104 <41029872+kimto8104@users.noreply.github.com>
2025-01-06 20:56:22 +0900

    下タブを一時コメントアウト

--- NowFocus/MainContentView.swift
+++ NowFocus/MainContentView.swift
@@ -8,24 +8,26 @@
 import SwiftUI
 
 struct MainContentView: View {
-  @State private var selectedTab: TabIcon = .Home // タブの状態を管理
+  //  @State private var selectedTab: TabIcon = .Home // タブの状態を管理
+  //  @State var showAlert: Bool = false
   var body: some View {
     GeometryReader { gp in
       let hm = gp.size.width / 375
       let vm = gp.size.height / 667
       let multiplier = abs(hm - 1) < abs(vm - 1) ? hm : vm
       // 現在のタブに応じたコンテンツを表示
-      TabContentView(selectedTab: selectedTab)
+      TimeSelectionView(presenter: TimeSelectionPresenter()) // 現在のビュー
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .edgesIgnoringSafeArea(.all) // コンテンツを画面全体に広げる
       
-      VStack {
-        Spacer()
-        TabBarView(selectedTab: $selectedTab, multiplier: multiplier)
-        Spacer()
-          .frame(height: 10 * multiplier)
-      }
-      .frame(width: gp.size.width)
+      
+      //      VStack {
+      //        Spacer()
+      //        TabBarView(selectedTab: $selectedTab, multiplier: multiplier)
+      //        Spacer()
+      //          .frame(height: 10 * multiplier)
+      //      }
+      //      .frame(width: gp.size.width)
     }
   }
 }
@@ -39,13 +41,11 @@ struct MainContentView_Previews: PreviewProvider {
 // コンテンツ切り替え用ビュー
 struct TabContentView: View {
   var selectedTab: TabIcon
-  
+  @Binding var showAlert: Bool
   var body: some View {
     switch selectedTab {
-    case .Home:
+    case .Home, .Clock:
       TimeSelectionView(presenter: TimeSelectionPresenter()) // 現在のビュー
-    case .Clock:
-      History()
     }
   }
 }
