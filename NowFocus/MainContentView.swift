627077e3056203c03ee3d8e7f9fee1262dc6f42a
tomo8104 <41029872+kimto8104@users.noreply.github.com>
2025-01-15 06:57:14 +0900

    最初の画面をTimerPageにするよう変更

--- NowFocus/MainContentView.swift
+++ NowFocus/MainContentView.swift
@@ -9,14 +9,13 @@ import SwiftUI
 
 struct MainContentView: View {
   @State private var selectedTab: TabIcon = .Home // タブの状態を管理
-  @State private var isTimerPageActive: Bool = false
   var body: some View {
     GeometryReader { gp in
       let hm = gp.size.width / 375
       let vm = gp.size.height / 667
       let multiplier = abs(hm - 1) < abs(vm - 1) ? hm : vm
       // 現在のタブに応じたコンテンツを表示
-      TabContentView(selectedTab: selectedTab, isTimerPageActive: $isTimerPageActive)
+      TabContentView(selectedTab: selectedTab)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .edgesIgnoringSafeArea(.all) // コンテンツを画面全体に広げる
       VStack {
@@ -25,8 +24,6 @@ struct MainContentView: View {
         Spacer()
           .frame(height: 10 * multiplier)
       }
-      .opacity(isTimerPageActive ? 0 : 1)
-      .animation(.easeInOut, value: isTimerPageActive)
       .frame(width: gp.size.width)
     }
   }
@@ -41,11 +38,10 @@ struct MainContentView_Previews: PreviewProvider {
 // コンテンツ切り替え用ビュー
 struct TabContentView: View {
   var selectedTab: TabIcon
-  @Binding var isTimerPageActive: Bool
   var body: some View {
     switch selectedTab {
     case .Home:
-      TimeSelectionView(presenter: TimeSelectionPresenter(), isTimerPageActive: $isTimerPageActive) // 現在のビュー
+      TimerRouter.initializeTimerModule(with: 1)
     case .Clock:
       HistoryPage()
     }
